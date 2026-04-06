68794097ce7ae448a03fe663ffaa556c869a571f
Donietta <donietta.randon@ensea.fr>
2026-04-06 22:50:36 +0200

    commit de generateSVG

---
+++ src/bin/generateSVG.rs
@@ -0,0 +1,20 @@
+
+use svg_fmt::*;
+
+
+fn main() {
+    println!("{}", BeginSvg { w: 800.0, h: 600.0 });
+    println!("    {}",
+             rectangle(20.0, 50.0, 200.0, 100.0)
+                 .fill(Fill::Color(red()))
+                 .stroke(Stroke::Color(black(), 3.0))
+                 .border_radius(5.0)
+    );
+    println!("    {}",
+             text(25.0, 100.0, "Hi!")
+                 .size(42.0)
+                 .color(white())
+    );
+    println!("{}", EndSvg);
+}
+
